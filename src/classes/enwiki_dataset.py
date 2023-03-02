bb0dd490f765936cea4a745d562c649e8b17dbc4
Genie <frothsavd@gmail.com>
2023-03-02 12:32:51 -0500

    Refactor dataset logic

---
+++ src/classes/enwiki_dataset.py
@@ -0,0 +1,247 @@
+import json
+import re
+from pathlib import Path
+from random import random
+from typing import Tuple
+
+from torch.utils.data import DataLoader2, Dataset
+from torchtext import datasets
+from tqdm import tqdm
+
+from config import paths
+
+Lines = list[str]
+VocabTally = dict[str, int]
+
+
+class EnwikiDataset(Dataset):
+    """Cleaned version of http://mattmahoney.net/dc/textdata.html
+
+    Occupies ~3 GB on disk
+    """
+
+    lines: list[list[str]]
+    vocab: VocabTally
+
+    vocab_to_index: dict[str, int]
+    index_to_vocab: dict[int, str]
+
+    def __init__(self, lines: Lines, vocab: VocabTally, sequence_length: int):
+        super().__init__()
+
+        self.lines = [l.split() for l in lines]
+        self.vocab = vocab
+        self.sequence_length = sequence_length
+        self.vocab_to_index = {w: i for i, w in enumerate(vocab)}
+        self.index_to_vocab = {i: w for i, w in enumerate(vocab)}
+
+    @classmethod
+    def load(cls, sequence_length: int, freq_thresh: int = 100) -> "EnwikiDataset":
+        lines, vocab = cls._constrain(freq_thresh, sequence_length)
+
+        ds = EnwikiDataset(lines, vocab, sequence_length)
+        print(
+            f"Loaded Enwiki dataset with with {len(lines):,} sentences and {len(vocab):,} words and {sequence_length=}"
+        )
+
+        return ds
+
+    def __getitem__(self, index):
+        words = self.lines[index]
+
+        maxStart = len(words) - (self.sequence_length + 1)
+        start = round(random() * maxStart)
+        end = start + self.sequence_length
+        sample = words[start : end - 1]
+        label = words[end - 1]
+
+        sample_idx = [self.vocab_to_index[w] for w in sample]
+        label_idx = self.vocab_to_index[label]
+
+        return sample_idx, label_idx
+
+    def __len__(self):
+        return len(self.lines)
+
+    @classmethod
+    def _constrain(
+        cls, freq_thresh: int, length_thresh: int
+    ) -> Tuple[Lines, VocabTally]:
+        """
+        Reduce dataset size by...
+            - replacing words that rarely appear with <UNK>
+            - omitting short sentences
+
+        This saves an additional copy of the data (~0.9 GB) and vocab (~0.1 GB) to disk
+        """
+
+        def constrain(
+            lines: Lines, vocab: VocabTally, freq_thresh: int, length_thresh: int
+        ):
+            UNK = "<UNK>"
+
+            lines_filtered = lines.copy()
+            vocab_filtered = dict()
+            vocab_filtered[UNK] = 0
+
+            whitelist = set(k for k, v in vocab.items() if v >= freq_thresh)
+
+            print("Constraining vocab...")
+            for l in tqdm(lines):
+                words = l.split()
+
+                if len(words) < length_thresh:
+                    continue
+
+                for i, w in enumerate(words):
+                    if w not in whitelist:
+                        words[i] = "<UNK>"
+                        vocab_filtered[UNK] += 1
+                    else:
+                        vocab_filtered.setdefault(w, 0)
+                        vocab_filtered[w] += 1
+
+                lines_filtered.append(" ".join(words))
+
+            return (lines_filtered, vocab_filtered)
+
+        def constrain_and_save(
+            lines: Lines,
+            vocab: VocabTally,
+            freq_thresh: int,
+            length_thresh: int,
+            fp_lines: Path,
+            fp_vocab: Path,
+        ) -> Tuple[Lines, VocabTally]:
+            lines, vocab = constrain(lines, vocab, freq_thresh, length_thresh)
+
+            line_data = dict(data=lines, meta=dict(length_thresh=length_thresh))
+            with open(fp_lines, "w+") as file:
+                json.dump(line_data, file, indent=2)
+
+            vocab_data = dict(data=vocab, meta=dict(freq_thresh=freq_thresh))
+            with open(fp_vocab, "w+") as file:
+                json.dump(vocab_data, file, indent=2)
+
+            return lines, vocab
+
+        def create():
+            lines, vocab = cls._preprocess()
+            return constrain_and_save(
+                lines, vocab, freq_thresh, length_thresh, fp_lines, fp_vocab
+            )
+
+        fp_lines = paths.DATASET_DIR / "enwiki_lines_constrained.json"
+        fp_vocab = paths.DATASET_DIR / "enwiki_vocab_constrained.json"
+
+        if not fp_lines.exists() or not fp_vocab.exists():
+            return create()
+        else:
+            # Load from file and validate
+            with open(fp_lines) as file:
+                lines_data = json.load(file)
+                if lines_data["meta"]["length_thresh"] != length_thresh:
+                    return create()
+            with open(fp_vocab) as file:
+                vocab_data = json.load(file)
+                if vocab_data["meta"]["freq_thresh"] != freq_thresh:
+                    return create()
+
+        return lines_data["data"], vocab_data["data"]
+
+    @classmethod
+    def _preprocess(cls) -> Tuple[Lines, VocabTally]:
+        """
+        This function will...
+            - download the dataset (~1.2 GB)
+            - clean up unwanted characters and store a copy (~0.9 GB)
+            - build a frequency count for each word (~0.1 GB)
+        """
+
+        fp_lines = paths.DATASET_DIR / "enwiki_lines.json"
+        if fp_lines.exists():
+            with open(fp_lines) as file:
+                lines = json.load(file)
+        else:
+            print("Downloading dataset...")
+            pipe = datasets.EnWik9(str(paths.DATA_DIR))
+            loader = DataLoader2(pipe)
+
+            print("Cleaning data...")
+            lines = cls._clean_data(fp_lines, loader)
+
+        fp_vocab = paths.DATASET_DIR / "enwiki_vocab.json"
+        if fp_vocab.exists():
+            with open(fp_vocab) as file:
+                vocab = json.load(file)
+        else:
+            print("Counting vocab...")
+            vocab = cls._tally_vocab(fp_vocab, " ".join(lines))
+
+        return (lines, vocab)
+
+    @classmethod
+    def _clean_data(cls, fp: Path, dataloader) -> Lines:
+        """Join sentences and remove unwanted characters"""
+
+        def main():
+            if fp.exists():
+                with open(fp) as file:
+                    return json.load(file)
+
+            pbar = tqdm(dataloader)
+            lines = []
+            for l in pbar:
+                cleaned = clean(" ".join(l))
+                if cleaned:
+                    lines.append(cleaned)
+
+            with open(fp, "w+") as file:
+                json.dump(lines, file, indent=2)
+
+            return lines
+
+        def clean(text: str) -> str:
+            result = text.strip()
+
+            # Convert line-breaks to spaces
+            result = result.replace("\n", " ")
+
+            # Replace periods with <END>
+            result = re.sub(r"\. ", " zENDz ", result)
+
+            # Remove multi-spaces
+            result = re.sub(r" +", " ", result)
+
+            # Remove multi-quotes
+            result = re.sub(r"'+", "'", result)
+
+            # Remove other characters
+            result = re.sub("[^a-z0-9' ]", "", result, flags=re.IGNORECASE)
+
+            return result
+
+        return main()
+
+    @classmethod
+    def _tally_vocab(cls, fp: Path, text: str) -> VocabTally:
+        """Get unique words and their frequency count"""
+
+        if fp.exists():
+            with open(fp) as file:
+                return json.load(file)
+
+        vocab = dict()
+        words = text.split()
+
+        for w in tqdm(words):
+            w = w.lower()
+            vocab.setdefault(w, 0)
+            vocab[w] += 1
+
+        vocab_sorted = dict(sorted(vocab.items(), key=lambda it: it[1], reverse=True))
+
+        with open(fp, "w+") as file:
+            json.dump(vocab_sorted, file, indent=2)
+
+        return vocab_sorted
--- src/tools/run_nnlm.py
+++ src/tools/run_nnlm.py
@@ -1,15 +1,6 @@
-import json
-import re
-from collections import OrderedDict
-from pathlib import Path
-from typing import Tuple
-
 from torch import nn
-from torch.utils.data import DataLoader2
-from torchtext import datasets
-from tqdm import tqdm
 
-from config import paths
+from classes.enwiki_dataset import EnwikiDataset
 
 Lines = list[str]
 VocabTally = dict[str, int]
@@ -33,7 +24,7 @@ class Nnlm(nn.Module):
         self.H = nn.Linear((self.n - 1) * self.params_feature, self.params_hidden)
 
         # output layer weights
-        self.U = nn.Linear(self.params_feature)
+        self.U = nn.Linear(self.params_hidden, self.params_feature)
 
     def forward(self, xb):
         # convert words to embeddings
@@ -53,124 +44,9 @@ class Nnlm(nn.Module):
         return out
 
 
-def get_clean_data(fp: Path, dataloader: DataLoader2) -> list:
-    """Remove unwanted characters"""
-
-    def main():
-        if fp.exists():
-            with open(fp) as file:
-                return json.load(file)
-
-        pbar = tqdm(dataloader)
-        lines = []
-        for l in pbar:
-            cleaned = clean(" ".join(l))
-            if cleaned:
-                lines.append(cleaned)
-
-        with open(fp, "w+") as file:
-            json.dump(lines, file, indent=2)
-
-        return lines
-
-    def clean(text: str) -> str:
-        result = text.strip()
-
-        # Convert line-breaks to spaces
-        result = result.replace("\n", " ")
-
-        # Replace periods with <END>
-        result = re.sub(r"\. ", " zENDz ", result)
-
-        # Remove multi-spaces
-        result = re.sub(r" +", " ", result)
-
-        # Remove multi-quotes
-        result = re.sub(r"'+", "'", result)
-
-        # Remove other characters
-        result = re.sub("[^a-z0-9' ]", "", result, flags=re.IGNORECASE)
-
-        return result
-
-    return main()
-
-
-def get_vocab(fp: Path, text: str) -> VocabTally:
-    """Get unique words and their frequency count"""
-
-    if fp.exists():
-        with open(fp) as file:
-            return json.load(file)
-
-    vocab = dict()
-    words = text.split()
-
-    for w in tqdm(words):
-        w = w.lower()
-        vocab.setdefault(w, 0)
-        vocab[w] += 1
-
-    vocab_sorted = dict(sorted(vocab.items(), key=lambda it: it[1], reverse=True))
-
-    with open(fp, "w+") as file:
-        json.dump(vocab_sorted, file, indent=2)
-
-    return vocab_sorted
-
-
-def constrain(
-    lines: Lines, vocab: VocabTally, freq_thresh: int, length_thresh: int
-) -> Tuple[Lines, VocabTally]:
-    """
-    Reduce dataset size by...
-        - replacing words that rarely appear with <UNK>
-        - omitting short sentences
-    """
-
-    UNK = "<UNK>"
-
-    lines = lines.copy()
-    vocab = vocab.copy()
-    vocab[UNK] = 0
-
-    whitelist = set(k for k, v in vocab.items() if v >= freq_thresh)
-
-    for i, l in enumerate(tqdm(lines)):
-        result = l
-        words = l.split()
-
-        if len(words) < length_thresh:
-            continue
-
-        for w in words:
-            if w not in whitelist:
-                result = result.replace(w, "<UNK>")
-                vocab[UNK] += 1
-
-        lines[i] = result
-
-    return (lines, vocab)
-
-
 if __name__ == "__main__":
-    print("Loading dataset")
-    pipe = datasets.EnWik9(str(paths.DATA_DIR))
-    loader = DataLoader2(pipe, shuffle=True, drop_last=True)
-
-    print("Loading vocab")
-    lines = get_clean_data(paths.DATASET_DIR / "enwiki_data.jsonc", loader)
-    vocab = get_vocab(paths.DATASET_DIR / "enwiki_vocab.jsonc", " ".join(lines))
-
-    print("Constraining data")
     sequence_length = 5
-    lines, vocab = constrain(lines, vocab, 100, sequence_length)
+    freq_thresh = 100
 
-    print("Initializing dataset")
-    vocab_to_index = {w: i for i, w in enumerate(vocab)}
-    index_to_vocab = {i: w for i, w in enumerate(vocab)}
-
-    print(
-        f"Training with {len(lines):,} sentences and {len(vocab):,} words and {sequence_length=}"
-    )
-    model = Nnlm(len(vocab), sequence_length)
+    ds = EnwikiDataset.load(sequence_length, freq_thresh)
+    model = Nnlm(len(ds.vocab), sequence_length)
